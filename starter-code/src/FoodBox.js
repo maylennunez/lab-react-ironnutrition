deca8bdabc9a217a77836bbdeaf2372a83c866ac
Maylen Nunez <maylennunez@hotmail.com>
2020-03-11 09:11:30 -0400

    add search bar

---
+++ starter-code/src/FoodBox.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+export default class FoodBox extends Component {
+
+
+
+
+
+  state = {
+    quantity: 0
+  }
+
+  handleQuantityChange = (event) => {
+    // check out what the event is giving us
+    // console.log(typeof event.target)
+    // console.log(event.target)
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    // console.log(typeof event.target.value)
+    let quantityFromInput = event.target.value
+
+
+    this.setState({
+      quantity: quantityFromInput
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  render() {
+    // console.log(this.props)
+    return (
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={this.props.image} alt={this.props.name}/>
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{this.props.name}</strong> <br />
+                <small>{this.props.calories} cal</small>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input
+                  className="input"
+                  type="number" 
+                  // value="1"
+                  onChange={this.handleQuantityChange}
+                />
+              </div>
+              <div className="control">
+                {/* <button className="button is-info"> */}
+                <button className="button is-info" onClick={() => this.props.addFoodToTodaysList(this.props.name, this.props.calories, this.state.quantity)}>
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    )
+  }
+}
---
+++ starter-code/src/search.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+ export default class search extends Component {
+   state = {
+     input: ""
+   }
+
+   handleSearch = event => {
+     // const { value } = event.target;
+     // console.log(event.target.value)
+     let value = event.target.value;
+     this.props.filterFoods(value);
+     this.setState({
+       input: value
+     });
+   };
+   render() {
+     return (
+       <div>
+         <input type="text" onChange={this.handleSearch} placeholder="Search ..." value={this.state.input}/>
+       </div>
+     )
+   }
+ }
